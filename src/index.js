e849521137d17e64bbfc2db1373df6b9388c2d1c
Herman Kudria <h.kudrya@hotmail.com>
2022-06-29 15:43:08 +0200

    feat: write program logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const length = str.length
+    for (let i = 0; i <= length; i++) {
+        for (const config of bracketsConfig) {
+            let strConfig = config.join('')
+            str = str.replace(strConfig,'')
+        }
+    }
+   return !str
 }
